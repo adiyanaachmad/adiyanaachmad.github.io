7a7f0a055137d21def5008debb507813030309f2
adiyanaachmad <acreative3179@gmail.com>
2025-04-14 21:08:24 +0700

    Perbaikan_bug_&_3d_model

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,13 +16,14 @@ if (navClose) {
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
 
-const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // Tutup menu mobile
+        document.getElementById('nav-menu').classList.remove('show-menu');
+    });
+});
 
 /*=============== SHADOW HEADER ===============*/
 const shadowHeader = () => {
@@ -44,24 +45,30 @@ const scrollUp = () => {
 window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+
+const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
-    const scrollY = window.pageYOffset
+    const scrollY = window.pageYOffset;
 
     sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 50,
-            sectionId = current.getAttribute('id')
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        const navLink = document.querySelector('#nav-menu a[href*=' + sectionId + ']');
+
+        if (navLink) {
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                navLink.classList.add('active-link');
+            } else {
+                navLink.classList.remove('active-link');
+            }
         }
-    })
+    });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener('scroll', scrollActive);
+
 
 /*=============== DARK LIGHT THEME ===============*/
 const themeButtons = document.querySelectorAll('.change-theme')
